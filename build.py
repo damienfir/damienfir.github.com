ca9e6173ccb77d956b36ae98a9003c966448a6c4
Damien Firmenich <fir.damien@gmail.com>
2021-09-14 09:04:51 +0200

    publish 20211409-090451

--- build.py
+++ build.py
@@ -5,17 +5,22 @@ from datetime import datetime
 from feedgen.feed import FeedGenerator
 import sys
 
+home_link = """
+<div><a href="/">Back</a></div>
+"""
+
 build_folder = sys.argv[1]
 last_update = datetime.now().strftime("%Y-%m-%d")
 fg = FeedGenerator()
 fg.title("Damien Firmenich")
-fg.link( href='http://example.com', rel='alternate' )
+fg.link(href='http://example.com', rel='alternate')
 fg.description("asdfsa")
 
 with open("template.html") as fp:
     template = fp.read()
 
 for post_name in os.listdir("pages") + [""]:
+    home = home_link if post_name else ""
     post_folder = os.path.join("pages", post_name)
     if not os.path.isdir(post_folder):
         continue
@@ -29,14 +34,13 @@ for post_name in os.listdir("pages") + [""]:
     out_folder = os.path.join(build_folder, post_name)
     os.makedirs(out_folder, exist_ok=True)
 
-    templated = template.format(content=content, last_update=last_update, **meta)
+    content = content.format(last_update=last_update)
+    templated = template.format(content=content, home=home, **meta)
     with open(os.path.join(out_folder, "index.html"), 'w') as fp:
         fp.write(templated)
-    
+
     fe = fg.add_entry()
     fe.title(post_name)
     fe.description("test")
-
-
 
 fg.rss_file(os.path.join(build_folder, "rss.xml"), pretty=True)
--- pages/index.md
+++ pages/index.md
@@ -1,19 +1,18 @@
 # Damien Firmenich
 
-## Blog
 
-- [Weekly digest 37](20210913-weekly-digest-37)
-- Zig raytracing in one weekend
-- Cards 13 solitaire
-- CppLisp
-- DLisp (Julia)
-- Tetris
-- Asteroids
-- Pacman
-- Pong
-- Snake
+## 2021
 
-<p><a href="rss.xml"><img src="static/rss.png" width="16px"></img> RSS</a></p>
-<p><a href="https://github.com/damienfir"><img src="static/GitHub-Mark-32px.png" width="16px"></img> damienfir</a></p>
-<p><a href="https://twitter.com/dfirmenich"><img src="static/twitter.png" width="16px"></img> dfirmenich</a></p>
-<p><a href="mailto:fir.damien@gmail.com"><img src="static/gmail.png" width="16px"></img> fir.damien@gmail.com</a></p>
++--------+-----------------------------------------------+
+| 13 Sep | [Weekly digest 37](20210913-weekly-digest-37) |
+
+
+## Where to find me
+
+<p><a href="rss.xml"> RSS feed</a></p>
+<p>github <a href="https://github.com/damienfir">damienfir</a></p>
+<p>twitter <a href="https://twitter.com/dfirmenich">dfirmenich</a></p>
+<p>email <fir.damien@gmail.com></p>
+
+
+#### Last updated: {last_update}
